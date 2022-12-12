56b90f6382f5e2ecc6b0191490f7ee1df7923522
Oleksandra <ozvoryhina@kse.org.ua>
2022-12-12 20:22:41 +0200

    upgrated_task1 - command line arguments

--- assignment7.py
+++ assignment7.py
@@ -1,26 +1,37 @@
-def split_line(line):
-    line_list = line.split()
-    result = []
-    index = 0
-    paws_met = False # перевіряємо лапки
-    for element in line_list:
-        if element.startswith('"'):
-            result[]
+import argparse
 
-def checking_medals(medals, country, ):
-    with open("Olympic Athletes - athlete_events.csv", "r") as file:
-        head = None
-        for line in file.readlines():
-            if head is None:
-                head = line.split(",")
-                continue
-            line.split()
+parser = argparse.ArgumentParser(description="our example parser.")
+parser.add_argument("--filename", "-f", required=True)
+parser.add_argument('--output', required=False)
+parser.add_argument("--medals", action="store_true", required=False)
+parser.add_argument('--country', required=False)
+parser.add_argument('--year', required=False)
+
+args = parser.parse_args()
+
+import sys
 
 
-p1 = ""
-p2 = ""
+if args.medals:
+    output_file = None
+    row = 0
+    if args.output is not None:
+        output_file = open(args.output, "w")
+    with open(args.filename, "r") as file:
+        for line in file:
+            data = line.strip().split("\t")
+            if data[7] == args.country and data[9] == args.year:
+                if output_file is not None and row < 10:
+                    row += 1
+                    output_file.write(f"{data[1]}, {data[-2]}, {data[-1]}\n")
 
-while True:
-    p1 = input()
-    p2 = input()
-    checking_medals(p1, p2)
+
+
+
+#args = parser.parse_args()
+print(f"{args.filename=}")
+print(f"{args.medals=}")
+print(f"{args.year=}")
+print(f"{args.country=}")
+
+print(args)
